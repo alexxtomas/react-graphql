eda1545b5031189cea8b1a31ce49f54d9795c7dd
Alex Tomas <atomasllimos@gmail.com>
2022-07-27 22:45:10 +0200

    Using ApolloProvider and useQuery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+// Importamos el hook useQuery
+import {gql, useQuery} from '@apollo/client'
+import Persons from './Persons'
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      id
+      name
+      phone
+      address {
+        street
+        city
+      }
+
+    }
+  }
+`
 
 function App() {
- 
+  /* Utilizamos el useQuery y le pasamos la query que queremos realizar dentro de result tenemos el resultado de la query */
+  const {data, error, loading} = useQuery(ALL_PERSONS)
 
+  // Si hay un error mostramos el error
+  if (error) return <span style='color: red'>{error}</span>
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>GraphQl + React</h1>
+      {
+        // Si el loading no se ha completado mostramos Loading... y si no mostramos el componente Persons
+      loading 
+      ? <p>Loading...</p> 
+      : <Persons persons={data?.allPersons}/>
+      }
+      
     </div>
   )
 }
---
+++ src/Persons.jsx
@@ -0,0 +1,12 @@
+
+const Persons = ({persons}) => {
+    if (persons === null) return null
+    return (
+        <div>
+            <h2>Persons</h2>
+            {persons.map(p => <div key={p.id}>{p.name} {p?.phone}</div> )}
+        </div>
+    )
+}
+
+export default Persons
